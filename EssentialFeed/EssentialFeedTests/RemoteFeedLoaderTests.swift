a0ddf747c1a1a05892ec5310a6eaa15f428c6ab3
kamran.namazov <kamran.namazov@limestonedigital.com>
2024-11-27 10:37:02 +0400

    fix: refactored and separated helper and presedence

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -22,37 +22,39 @@ class RemoteFeedLoader {
     }
 }
 
+
 protocol HTTPClient {
     func get(from url: URL)
 }
 
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
 
 final class RemoteFeedLoaderTests: XCTestCase {
-
+    
     func test_init_doesNotRequestFromURL() {
         let(_, client) = makeFactorySUT()
         XCTAssertNil(client.requestedURL)
     }
-
-  
+    
     func test_load_requestDataFromURL() {
         let url = URL(string: "/url")!
-
         let (sut, client) = makeFactorySUT()
         sut.load()
         XCTAssertEqual(client.requestedURL, url)
     }
     
-    func makeFactorySUT(url: URL = URL(string: "/url")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    // MARK:  - Helpers
+    private func makeFactorySUT(url: URL = URL(string: "/url")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(client: client, url: url)
         return (sut, client)
     }
+    
+    private  class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+    
 }
