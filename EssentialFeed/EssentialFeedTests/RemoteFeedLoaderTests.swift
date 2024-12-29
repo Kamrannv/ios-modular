ca3b6e09652518ec51d562167e951cc6bbb8194b
kamran.namazov <kamran.namazov@limestonedigital.com>
2024-12-29 15:39:19 +0400

    added invalid json test case

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -50,11 +50,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
             [RemoteFeedLoader.Error]()
             
             sut.load { capturedErrors.append($0) }
-            
             client.complete(withStatusCode: code, at: index)
             
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
+    }
+    
+    func test_loadDataWith200WithInvalidJson() {
+        let (sut, client) = makeFactorySUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        let invalidJSON = Data(bytes: "invalid".utf8)
+        
+        client.complete(withStatusCode: 200, data: invalidJSON)
     }
     // MARK:  - Helpers
     private func makeFactorySUT(url: URL = URL(string: "/url")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -78,14 +87,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code:Int, at index: Int = 0) {
+        func complete(withStatusCode code:Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedUrls[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil)!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
     
