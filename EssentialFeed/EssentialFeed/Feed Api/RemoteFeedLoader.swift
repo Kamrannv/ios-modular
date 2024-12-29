52c931ec9d6d1023525902b73f87ded8a7ac55c5
kamran.namazov <kamran.namazov@limestonedigital.com>
2024-12-29 10:11:37 +0400

    Delivers connectivity error

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error)->Void)
 }
 
 public final class RemoteFeedLoader {
@@ -17,13 +17,19 @@ public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
    public init(client: HTTPClient, url: URL = URL(string: "/url")!) {
         self.client = client
         self.url = url
     }
     
-   public func load() {
-        client.get(from:  url)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from:  url){ err in
+            completion(.connectivity)
+        }
     }
 }
 
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -31,6 +31,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeFactorySUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in capturedError = error }
+        XCTAssertEqual(capturedError, .connectivity)
+    }
     // MARK:  - Helpers
     private func makeFactorySUT(url: URL = URL(string: "/url")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -39,9 +47,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private  class HTTPClientSpy: HTTPClient {
- 
         var requestedURLs = [URL]()
-        func get(from url: URL) {
+        var error: Error?
+        func get(from url: URL, completion: @escaping (Error)->Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
