a68085670e2f46df7ff89e2b240d359c60efb978
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-01-02 14:26:50 +0400

    Add memory leak detection helper

--- EssentialFeed/EssentialFeed/Feed Api/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed Api/FeedItemsMapper.swift
@@ -21,7 +21,9 @@ internal final class FeedItemsMapper {
             return FeedItem(id: id, description: description, location: location, imageURL: image)
         }
     }
+    
     private static var OK_200: Int { return 200 }
+    
     internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
         guard  response.statusCode == OK_200 else {
             throw RemoteFeedLoader.HttpError.invalidData
