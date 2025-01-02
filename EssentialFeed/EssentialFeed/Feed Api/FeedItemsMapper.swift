69de3fbe693e86725dfaa3abf43b574b836d9c45
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-01-02 14:12:57 +0400

    internal and private access controls were added

--- EssentialFeed/EssentialFeed/Feed Api/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed Api/FeedItemsMapper.swift
@@ -21,8 +21,8 @@ internal final class FeedItemsMapper {
             return FeedItem(id: id, description: description, location: location, imageURL: image)
         }
     }
-    static var OK_200: Int { return 200 }
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+    private static var OK_200: Int { return 200 }
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
         guard  response.statusCode == OK_200 else {
             throw RemoteFeedLoader.HttpError.invalidData
         }
