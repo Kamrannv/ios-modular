d20fcef8a18cfad40387e28327f1314765429be2
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-01-06 18:04:01 +0400

    Helper was added to setup

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,20 +11,8 @@ import EssentialFeed
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGetFeedResult_matchesFixedAccountData() {
-        let serverURl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(client: client, url: serverURl)
-        
-        var receivedResult: LoadFeedResult?
-        let exp = expectation(description: "Wait for load completion")
-        loader.load { res in
-            receivedResult = res
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 5.0)
-        
-        switch receivedResult {
+    
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items loaded")
             items.enumerated().forEach { (index, item) in
@@ -39,6 +27,21 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     //MARK: Helpers
+    private func getFeedResult()->LoadFeedResult?{
+        let serverURl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(client: client, url: serverURl)
+        
+        var receivedResult: LoadFeedResult?
+        let exp = expectation(description: "Wait for load completion")
+        loader.load { res in
+            receivedResult = res
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
+    }
     private func expectedItems(at index:Int)->FeedItem {
         return FeedItem(id: id(at: index), description: description(at: index), location: location(at:index), imageURL: imageURL(at: index))
     }
