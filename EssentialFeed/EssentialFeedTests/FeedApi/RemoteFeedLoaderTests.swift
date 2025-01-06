f0d86f72a51665fe46fdfadb356398a3a0d0319e
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-01-06 11:49:51 +0400

    Memoryleadtracker was separated due to reusability

--- EssentialFeed/EssentialFeedTests/FeedApi/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedApi/RemoteFeedLoaderTests.swift
@@ -116,12 +116,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeaks(client, file:file, line:line)
         return (sut, client)
     }
-    
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
-        }
-    }
+ 
     
     private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: ()->Void, file: StaticString = #filePath, line: UInt = #line){
         
