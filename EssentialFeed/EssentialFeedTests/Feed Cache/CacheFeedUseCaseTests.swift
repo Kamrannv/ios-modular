ac103deec4b703efdda05168552633b851d3b2d8
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-02-28 14:21:06 +0400

    Extract FeedCacheSpy into a shared scope to remove duplication

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -8,43 +8,6 @@
 import XCTest
 import EssentialFeed
 
-
-private class FeedStoreSpy: FeedStore {
-    
-    enum ReceivedMessage: Equatable {
-        case deletedCachedFeed
-        case insert([LocaleFeedImage], Date)
-    }
-    private(set) var receivedMessages = [ReceivedMessage]()
-    
-    private var deletionCompletions = [DeletionCompletion]()
-    private var insertionCompletions = [InsertionCompletion]()
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
-        deletionCompletions.append(completion)
-        receivedMessages.append(.deletedCachedFeed)
-    }
-    
-    func completeDeletion(with error: Error, at index:Int = 0) {
-        deletionCompletions[index](error)
-    }
-    func completeDeletionSuccessfully(at index:Int = 0) {
-        deletionCompletions[index](nil)
-    }
-    func insert(_ items: [LocaleFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        insertionCompletions.append(completion)
-        receivedMessages.append(.insert(items, timestamp))
-    }
-    func completeInsertion(with error: Error, at index: Int = 0) {
-        insertionCompletions[index](error)
-    }
-    func completeInsertionSuccessfully(at index: Int = 0) {
-        insertionCompletions[index](nil)
-    }
-}
-
-//MARK: TEST
 final class CacheFeedUseCaseTests: XCTestCase {
     
     func test_doesNotMessageStoreUponInitialization() {
---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,43 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeed
+//
+//  Created by Kamran on 28.02.25.
+//
+import Foundation
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+    
+    enum ReceivedMessage: Equatable {
+        case deletedCachedFeed
+        case insert([LocaleFeedImage], Date)
+    }
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deletedCachedFeed)
+    }
+    
+    func completeDeletion(with error: Error, at index:Int = 0) {
+        deletionCompletions[index](error)
+    }
+    func completeDeletionSuccessfully(at index:Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    func insert(_ items: [LocaleFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(items, timestamp))
+    }
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
