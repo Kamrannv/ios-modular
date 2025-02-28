907e0ef56961e91819f0701d68de1ddc69f5944b
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-02-28 12:44:07 +0400

    FeedStore protocol was moved to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Kamran on 28.02.25.
+//
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (LocalFeedLoader.SaveResult)->Void
+    typealias InsertionCompletion = (LocalFeedLoader.SaveResult) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
