025b742cdf2372c402efdc43414d6f9d21740831
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-02-28 13:26:29 +0400

    Add RemoteFeedItem DTO to decouple the items mapper from FeedItem data models

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -17,10 +17,10 @@ public final class RemoteFeedLoader: FeedLoader {
         case invalidData
     }
     
-   public typealias Result = LoadFeedResult
- 
+    public typealias Result = LoadFeedResult
     
-   public init(client: HTTPClient, url: URL = URL(string: "/url")!) {
+    
+    public init(client: HTTPClient, url: URL = URL(string: "/url")!) {
         self.client = client
         self.url = url
     }
@@ -30,15 +30,29 @@ public final class RemoteFeedLoader: FeedLoader {
             guard self != nil else { return }
             switch result {
             case let .success(data, response):
-                completion(FeedItemsMapper.map(data, from: response))
+                completion(RemoteFeedLoader.map(data, from: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
-           
+            
         }
     }
-
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+    
 }
 
+extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        return map { FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) }
+    }
+}
 
 
