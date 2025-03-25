578a38863f7199dda9bc1f37db37189acdfd392d
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-03-25 18:09:40 +0400

    Move feed cache max age (7 days) to a computed var to clarify intent in code

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestsHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestsHelpers.swift
@@ -23,7 +23,10 @@ extension Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
     func minusFeedCacheMaxAge() -> Date {
-            return adding(days: -7)
+        return adding(days: -feedCacheMaxAgeInDays)
+        }
+    private var feedCacheMaxAgeInDays: Int {
+            return 7
         }
 
     func adding(seconds: TimeInterval) -> Date {
