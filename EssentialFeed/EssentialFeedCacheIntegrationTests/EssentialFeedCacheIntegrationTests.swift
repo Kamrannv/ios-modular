43e8925d3d678df7d7733ce495dbbb741c9946d0
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-03-27 16:13:39 +0400

    Include memory leak tracking helper in the cache integration tests target

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Kamran on 27.03.25.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+    func test_load_deliversNoItemsOnEmptyCache() {
+        
+        
+        
+    }
+
+}
