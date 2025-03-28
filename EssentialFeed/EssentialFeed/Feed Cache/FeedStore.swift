44af9b285a6eddaf509623a4d762f1d5b59d5773
kamran.namazov <kamran.namazov@limestonedigital.com>
2025-03-28 18:27:17 +0400

    Naming change for Cache result

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -6,7 +6,6 @@
 //
 import Foundation
 
-public typealias RetrieveCachedFeedResult = Result<CachedFeed, Error>
 
 public enum CachedFeed {
     case empty
@@ -15,7 +14,8 @@ public enum CachedFeed {
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?)->Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetreivalCompletion = (RetrieveCachedFeedResult) -> Void
+    typealias RetreivalResult = Result<CachedFeed, Error>
+    typealias RetreivalCompletion = (RetreivalResult) -> Void
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
