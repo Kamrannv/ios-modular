e65b3d40d100102140df4662be5ce8c6f95fd78b
kamran.namazov <kamranamazov@yahoo.com>
2025-03-30 15:48:52 +0400

    Extract UIRefreshControl test helper to a separate file

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -386,12 +386,4 @@ private extension FeedImageCell {
 }
 
  
-private extension UIRefreshControl {
-    func simulatePullToRefresh() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
+ 
