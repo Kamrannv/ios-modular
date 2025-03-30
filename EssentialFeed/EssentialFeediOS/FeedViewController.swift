3e29fd825f24c383e724cb882df3e12e0539cd5a
kamran.namazov <kamranamazov@yahoo.com>
2025-03-30 15:28:06 +0400

    Extract cancelImageDataLoad(from: URL) method from FeedImageDataLoader protocol into a new FeedImageDataLoaderTask protocol that represents a task that can be cancelled. This way, we respect the Interface Segregation Principle and `FeedImageDataLoader` implementations are not forced to be stateful.

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -7,56 +7,60 @@
 
 import UIKit
 import EssentialFeed
- 
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
 public protocol FeedImageDataLoader {
-    func loadImageData(from url: URL)
-    func cancelImageDataLoad(from url: URL)
+    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
     
     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         self.init()
         self.feedLoader = feedLoader
         self.imageLoader = imageLoader
     }
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
         load()
     }
-
+    
     @objc private func load() {
         refreshControl?.beginRefreshing()
         feedLoader?.load { [weak self] result in
             if let feed = try? result.get() {
-                            self?.tableModel = feed
-                            self?.tableView.reloadData()
+                self?.tableModel = feed
+                self?.tableView.reloadData()
             }
-                        self?.refreshControl?.endRefreshing()
-                }
+            self?.refreshControl?.endRefreshing()
+        }
     }
-     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return tableModel.count
-        }
-
-        public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cellModel = tableModel[indexPath.row]
-            let cell = FeedImageCell()
-            cell.locationContainer.isHidden = (cellModel.location == nil)
-            cell.locationLabel.text = cellModel.location
-            cell.descriptionLabel.text = cellModel.description
-            imageLoader?.loadImageData(from: cellModel.url)
-            return cell
-        }
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url)
+        return cell
+    }
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-            let cellModel = tableModel[indexPath.row]
-            imageLoader?.cancelImageDataLoad(from: cellModel.url)
-        }
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
+    }
 }
