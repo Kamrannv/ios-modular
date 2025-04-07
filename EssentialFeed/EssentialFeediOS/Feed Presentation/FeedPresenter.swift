9a4e859d81cd685364e907a7cf9bd334b902ae8f
kamran.namazov <kamranamazov@yahoo.com>
2025-04-07 14:43:57 +0400

    Move main thread dispatch to the Composition layer with a Decorator.

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -32,16 +32,12 @@ final class FeedPresenter {
         }
     
     func didStartLoadingFeed() {
-        guard Thread.isMainThread else {
-                    return DispatchQueue.main.async { [weak self] in self?.didStartLoadingFeed() }
-                }
+        
         loadingView.display(FeedLoadingViewModel(isLoading: true))
         
     }
     func didFinishLoadingFeed(with feed: [FeedImage]) {
-        guard Thread.isMainThread else {
-                    return DispatchQueue.main.async { [weak self] in self?.didFinishLoadingFeed(with: feed) }
-                }
+        feedView.display(FeedViewModel(feed: feed))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
     
